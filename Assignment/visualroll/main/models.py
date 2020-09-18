d90e7ff36f8f38c5bff76456292bcfe23c770b31
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-09-18 17:40:20 +1200

    Added models/views for group/user & started on templates.

--- Assignment/visualroll/main/models.py
+++ Assignment/visualroll/main/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
+
 class User(models.Model):
     email = models.TextField('Email')
     first_name = models.TextField('First Name')
     last_name = models.TextField('Last Name')
-    password = models.TextField('Password')
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+
+class Group(models.Model):
+    g_name = models.TextField('Group Name')
+    g_info = models.TextField('Group Info')
+    creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    date_created = models.DateField('Date Created', auto_now_add=True)
+    def __str__(self):
+        return self.g_name
+
 
--- Assignment/visualroll/main/views.py
+++ Assignment/visualroll/main/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to Visual Roll.")
+
+def register(request):
+    return HttpResponse("Welcome to Visual Roll.")
+def create_group(request):
+    return HttpResponse("Welcome to Visual Roll.")
