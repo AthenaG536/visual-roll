22219464e11fdf78fb6f243600610939e78c29c4
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-09-19 19:58:13 +1200

    Add new model and reference doc for other resources

--- Assignment/visualroll/main/models.py
+++ Assignment/visualroll/main/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.forms import forms
-from django.utils.datetime_safe import date
+from django.utils.datetime_safe import date, datetime
 
 
 class User(models.Model):
@@ -17,7 +17,12 @@ class Group(models.Model):
     g_name = models.CharField('Group Name', max_length=255)
     g_info = models.TextField('Group Info')
     creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    date_created = models.DateField('Date Created', auto_now_add=True, null=True)
+    date_created = models.DateField('Date Created', default=date.today, blank=True)
 
     def __str__(self):
         return self.g_name
+
+class GroupUser(models.Model):
+    user = models.ForeignObject(User, on_delete=models.CASCADE)
+    Group = models.ForeignObject(Group, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField('Date Joined', default=datetime.now, blank=True)
