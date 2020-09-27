879b040221941b67210db1043a1b52499448f8b0
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-09-27 21:27:40 +1300

    Editing to use uvicorn

--- Assignment/visualroll/main.py
+++ Assignment/visualroll/main.py
@@ -1,19 +1,35 @@
 import socket
 import sys
 
+import uvicorn
 from fastapi import FastAPI
+from rest_framework import routers
 
-app = FastAPI()
+# router = routers.SimpleRouter()
+#
+# app = FastAPI(docs_url=None,redoc_url=None)
+# app.include_router(router, prefix="/v1")
+#
+# hostname = socket.gethostname()
+#
+# version = f"{sys.version_info.major}.{sys.version_info.minor}"
+#
+#
+# @app.get("/")
+# async def read_root():
+#     return {
+#         "name": "visualroll",
+#         "host": hostname,
+#         "version": f"Hello world! From FastAPI running on Uvicorn. Using Python {version}"
+#     }
 
-hostname = socket.gethostname()
+class App:
+    async def __call__(self, scope, receive, send):
+        assert scope['type'] == 'http'
 
-version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
 
-@app.get("/")
-async def read_root():
-    return {
-        "name": "visualroll",
-        "host": hostname,
-        "version": f"Hello world! From FastAPI running on Uvicorn. Using Python {version}"
-    }
+app = App()
+
+if __name__ == "__main__":
+    uvicorn.run(app)
