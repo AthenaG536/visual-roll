de42722e4c365d64e1eb69b4d6a26a18219174b7
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-09-30 16:33:12 +1300

    Adding group_posts view

--- Assignment/visualroll/main/views.py
+++ Assignment/visualroll/main/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from main.models import Post
 
 
 def index(request):
@@ -13,6 +16,14 @@ def create_group(request):
 
 def view_group(request, group_id):
     return HttpResponse("Group name: %s" % group_id)
+
+def group_posts(request):
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    output = {
+        'latest_post_list': latest_post_list,
+    }
+    return HttpResponse(template.render(output, request))
 
 def view_photo(request, photo_id):
     return HttpResponse("Photo name: %s" % photo_id)
