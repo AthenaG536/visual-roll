b81c6685089136f54a34ddafcb0ee8819e66c3ca
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-02 07:02:54 +1300

    Updated Urls to have group/photo/member IDs

--- Assignment/visualroll/main/urls.py
+++ Assignment/visualroll/main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.index, name='index'),
-    path('group/<int:main.main_group.id>/', views.view_group, name='group'),
-    path('photo/<int:main.main_photo.id>/', views.view_photo, name='photo'),
-    path('photo/<int:main.main_member.id>/', views.members, name='member'),
+    path('group/<int:group_id>/', views.view_group, name='group'),
+    path('photo/<int:photo_id>/', views.view_photo, name='photo'),
+    path('photo/<int:member_id>/', views.members, name='member'),
 ]
