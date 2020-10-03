a14289fb47515ff2e44ac5126250d2ad6a6f294f
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-03 21:23:08 +1300

    Added more relevant views

--- Assignment/visualroll/main/views.py
+++ Assignment/visualroll/main/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
-from main.models import Post
+from main.models import Post, Group, Members, User, Likedphoto, Likedpost
 
 
 def index(request):
@@ -11,11 +11,18 @@ def index(request):
 def register(request):
     return HttpResponse("Welcome to Visual Roll.")
 
-def create_group(request):
-    return HttpResponse("Welcome to Visual Roll.")
+def user_groups(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    # members and group stuff
+    return render(request, 'user.html', {'user': user})
 
-def view_group(request, group_id):
-    return HttpResponse("Group name: %s" % group_id)
+def user_detail(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'user.html', {'user': user})
+
+def group_detail(request, group_id):
+    group = get_object_or_404(Group, pk=group_id)
+    return render(request, 'group.html', {'group': group})
 
 def group_posts(request):
     latest_post_list = Post.objects.order_by('-pub_date')[:5]
@@ -30,3 +37,24 @@ def view_photo(request, photo_id):
 
 def members(request, member_id):
     return HttpResponse("Member: %s" % member_id)
+
+#
+# def user_login(request):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = loginForm(request.POST)
+#         # check whether it's valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/thanks/')
+#
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         form = NameForm()
+#
+#     return render(request, 'name.html', {'form': form})
+#     return HttpResponse("Welcome to Visual Roll.")
+
