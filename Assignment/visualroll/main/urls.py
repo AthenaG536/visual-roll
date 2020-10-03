005971229b2295d81b730266234c61ecf4bebd67
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-03 21:23:56 +1300

    Updated urls to work dynamically. This took a while.

--- Assignment/visualroll/main/urls.py
+++ Assignment/visualroll/main/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/', views.index, name='index'),
-    path('group/<int:group_id>/', views.view_group, name='group'),
-    path('photo/<int:photo_id>/', views.view_photo, name='photo'),
-    path('photo/<int:member_id>/', views.members, name='member'),
+    path(r'', views.index, name='index'),
+    path(r'account/<int:user_id>/', views.user_detail, name='user'),
+    path(r'<int:user_id>/mygroups/', views.user_groups, name='user_groups'),
+    path(r'group/<int:group_id>', views.group_detail, name='group'),
+    path(r'photo/<int:photo_id>/', views.view_photo, name='photo'),
+    path(r'group/<int:group_id>/', views.group_detail, name='group'),
+    path(r'photo/<int:photo_id>/', views.view_photo, name='photo'),
 ]
