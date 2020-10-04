0efd373fb1d6959fce2db3d44a326ad48669de48
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-04 21:31:15 +1300

    commiting all

--- visualroll/asgi.py
+++ visualroll/asgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+import uvicorn
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visualroll.settings')
 
 application = get_asgi_application()
+
+if __name__ == '__main__':
+    uvicorn.run(application, host='https://visualroll.herokuapp.com/')
