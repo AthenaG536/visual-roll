29f42f4add9bdcf0d51b5c195b23fb9afbce08b7
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-04 14:35:06 +1300

    Dividing index into home, index, welcome, register and login. & updating views

--- Assignment/visualroll/main/views.py
+++ Assignment/visualroll/main/views.py
@@ -1,24 +1,40 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.views import generic
 
 from main.models import Post, Group, Members, User, Likedphoto, Likedpost
 
 
+class UserView(generic.DetailView):
+    model = User
+    template_name = 'user.html'
+
+class GroupView(generic.ListView):
+    template_name = 'group.html'
+    context_object_name = 'group_list'
+
+    def get_queryset(self):
+        """Return groups by date created."""
+        return Group.objects.order_by('-date_created')
+
 def index(request):
     return render(request, 'index.html')
 
+def home(request):
+    return render(request, 'welcome.html')
+
+def login(request):
+    return render(request, 'login.html')
+
 def register(request):
-    return HttpResponse("Welcome to Visual Roll.")
+    return render(request, 'register.html')
 
 def user_groups(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     # members and group stuff
     return render(request, 'user.html', {'user': user})
 
-def user_detail(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    return render(request, 'user.html', {'user': user})
 
 def group_detail(request, group_id):
     group = get_object_or_404(Group, pk=group_id)
