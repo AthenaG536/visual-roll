4d7096eb566a0069534af3f0dbf9c5ed74c0a4f5
Athena <64632419+AthenaG536@users.noreply.github.com>
2020-10-04 14:35:30 +1300

    Dividing index into home, index, welcome, register and login. & updating views

--- Assignment/visualroll/main/urls.py
+++ Assignment/visualroll/main/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path(r'', views.index, name='index'),
-    path(r'account/<int:user_id>/', views.user_detail, name='user'),
+    # TODO: swap home and index templates around to be accurate.
+    path(r'', views.home, name='index'),
+    path(r'home/', views.index, name='home'),
+    path(r'login/', views.login, name='login'),
+    path(r'register/', views.register, name='register'),
+    path(r'account/<int:pk>/', views.UserView.as_view(), name='user'),
     path(r'<int:user_id>/mygroups/', views.user_groups, name='user_groups'),
     path(r'group/<int:group_id>', views.group_detail, name='group'),
-    path(r'photo/<int:photo_id>/', views.view_photo, name='photo'),
-    path(r'group/<int:group_id>/', views.group_detail, name='group'),
     path(r'photo/<int:photo_id>/', views.view_photo, name='photo'),
 ]
